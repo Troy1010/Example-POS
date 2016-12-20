9a64fb4a5fc79c825426f0a5f35f1f556d51c32e
Troy1010 <tminus1010@gmail.com>
2016-12-19 22:03:46 -0600

    Enumerable Ext

---
+++ ExamplePOS/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplePOS
+{
+    class Menu
+    {
+        public List<MenuItem> Items = new List<MenuItem>();
+        public string Narrate2()
+        {
+            // This version is to be read by user
+            string sReturning = "";
+            int iTabCount = 0;
+            foreach (var menuItem in this.Items.SkipFirst())
+            {
+                if (!menuItem.Item2)
+                {
+                    sReturning += "\n";
+                }
+                iTabCount = Math.Max(iTabCount, (int)Math.Ceiling(menuItem.Item1.Name.Length / 4m));
+                sReturning += menuItem.Item1.Name + "<TAB>" + menuItem.Item1.BasePrice;
+            }
+            sReturning = sReturning.Replace("<TAB>",new string('\t',iTabCount));
+            return sReturning;
+        }
+        public string Narrate()
+        {
+            // This version is to be read by developer
+            string sReturning = "";
+            foreach (var menuItem in this.Items.SkipFirst())
+            {
+                if (!menuItem.Item2)
+                {
+                    sReturning += "\n";
+                }
+                sReturning += menuItem.Item1.Name;
+            }
+            return sReturning;
+        }
+    }
+    class MenuItem
+    {
+        public string Name;
+        public decimal BasePrice;
+        public decimal Price
+        {
+            get { return BasePrice; }
+        }
+    }
+    static class EnumeratorExt
+    {
+        public static IEnumerable<Tuple<T,bool>> SkipFirst<T>(this IEnumerable<T> vLoopable)
+        {
+            bool bDoOnce = false;
+            foreach (var item in vLoopable)
+            {
+                if (!bDoOnce)
+                {
+                    bDoOnce = true;
+                    yield return new Tuple<T, bool>(item,true);
+                }
+                else
+                {
+                    yield return new Tuple<T, bool>(item, false);
+                }
+            }
+        }
+    }
+}
--- ExamplePOS/Program.cs
+++ ExamplePOS/Program.cs
@@ -10,6 +10,13 @@ namespace ExamplePOS
     {
         static void Main(string[] args)
         {
+            Menu oMenu = new Menu();
+            oMenu.Items.Add(new MenuItem { Name = "Blonde Roast Coffee", BasePrice = 4.03m });
+            oMenu.Items.Add(new MenuItem { Name = "Earl Grey Tea", BasePrice = 1.34m });
+            oMenu.Items.Add(new MenuItem { Name = "Biscuit", BasePrice = 3.21m });
+            oMenu.Items.Add(new MenuItem { Name = "Turkey Sandwich", BasePrice = 2.21m });
+            Console.WriteLine(oMenu.Narrate2());
+            Console.ReadLine();
         }
     }
 }
